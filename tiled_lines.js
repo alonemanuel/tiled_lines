df995eae134480897a2c00f29202906c2c30e2b2
Alon Emanuel <alon@alonemanuel.com>
2020-02-15 08:52:23 +0200

    First running html

--- tiled_lines.js
+++ tiled_lines.js
@@ -5,7 +5,19 @@ var size = window.innerWidth;
 var dpr = window.devicePixelRatio;
 canvas.width = size * dpr;
 canvas.height = size * dpr;
-context.scale(dpr * dpr);
+context.scale(dpr , dpr);
 
 context.lineCap = 'square';
-context.lineWidth = 2;
+context.lineWidth = 8;
+
+function draw(x, y, width, height) {
+    context.moveTo(x, y);
+    context.lineTo(x + width, y + height);
+    context.stroke()
+}
+
+draw(0, 0, size, size)
+draw(5, 5, size, size)
+
+
+
